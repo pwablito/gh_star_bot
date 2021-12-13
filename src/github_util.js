2a6c47da5c21ed261444a34d36d19e1bd0d4f98d
Spencer, Paul Whiting <pwablito@gmail.com>
2021-12-12 20:53:03 -0700

    ES module, network size constraints, add followers

--- src/github_util.js
+++ src/github_util.js
@@ -1,15 +1,22 @@
-module.exports = {
-    get_users: get_users,
-    get_starred_repos_for_user: get_starred_repos_for_user,
-};
+export const get_users = get_users;
+export const get_starred_repos_for_user = get_starred_repos_for_user;
 
-async function get_users(octokit_obj, username) {
+async function get_users(octokit_obj, username, network_size = 1) {
     let users = [];
     users.push(username);
-    let data = await octokit_obj.users.listFollowingForUser({
-        username: username
-    });
-    return users.concat(data.data.map(user => user.login));
+    for (let i = 0; i < network_size; i++) {
+        for (let user of users) {
+            let data = await octokit_obj.users.listFollowingForUser({
+                username: user.login
+            });
+            users = [...new Set(users.concat(data.data.map(user => user.login)))];
+            data = await octokit_obj.users.listFollowersForUser({
+                username: user.login
+            });
+            users = [...new Set(users.concat(data.data.map(user => user.login)))];
+        }
+    }
+    return users;
 }
 
 async function get_starred_repos_for_user(octokit_obj, username) {
